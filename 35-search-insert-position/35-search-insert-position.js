1ac78c3b194c3e383607cf2d9d2d9d9cde103999
YANG HAYUN <77730913+mycolki@users.noreply.github.com>
2022-01-15 16:22:54 +0900

    Time: 100 ms (26.59%), Space: 38.6 MB (95.43%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -5,7 +5,7 @@
  */
 var searchInsert = function(nums, target) {
   if (nums[0] > target) return 0;
-  if (nums[nums.length - 1] < target) return nums.length
+  if (nums[nums.length - 1] < target) return nums.length;
   
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] === target) return i;
