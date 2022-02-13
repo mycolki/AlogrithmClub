61f1216701f76b68ea3c5c77fda3e484df2c7613
YANG HAYUN <77730913+mycolki@users.noreply.github.com>
2022-02-13 15:09:16 +0900

    Time: 149 ms (42.77%), Space: 44.4 MB (23.70%) - LeetHub

--- 455-assign-cookies/455-assign-cookies.js
+++ 455-assign-cookies/455-assign-cookies.js
@@ -7,15 +7,15 @@ var findContentChildren = (children, cookies) => {
   children.sort((a, b) => a - b);
   cookies.sort((a, b) => a - b)
   
-  let childCount = children.length-1;
-  let cookieCount = cookies.length-1;
+  let childCount = children.length - 1;
+  let cookieCount = cookies.length - 1;
   let result = 0;
   
   while (childCount >= 0) {
     const isSatisfiedChild = cookies[cookieCount] >= children[childCount];;
       
     if (isSatisfiedChild){
-      result++
+      result++;
       childCount--;
       cookieCount--;
     } else {
